e5390c0b16a33a73890e538938b0307308c83c74
Florianeizd <floriane.izd45@gmail.com>
2023-07-05 15:11:05 +0200

    add scrapper

--- src/Api.php
+++ src/Api.php
@@ -1,14 +1,29 @@
 <?php
 
-declare(strict_types=1);
+namespace Flori\CssTd2;
 
-namespace Flori\csstd2;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpClient\HttpClient;
 
 class Api
 {
-    public function getRandomNumber(): int
+    public function httpRequest()
     {
-        return rand(0, 100);
-    }
+        $client = HttpClient::create();
+        $response = $client->request(
+            'GET',
+            'https://figurineharrypotter.com/liste-personnages'
+        );
+        $content = $response->getContent();
 
+        $html = $content;
+
+        $crawler = new Crawler($html);
+        $crawler->filter('.fiche > h2 > a')->each(function (Crawler $node, $i){
+            var_dump($node->text());
+        });
+
+
+
+    }
 }
